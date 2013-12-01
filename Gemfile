ba7d20c49669bf288e3077171d2b92d30dfbc12f
hmsk <k.hamasaki@gmail.com>
2013-12-01 19:08:26 +0900

    Create a minimum sample for payment with token

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'webpay'
+gem 'haml'
+
+ruby '2.0.0'
---
+++ app.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+require 'bundler'
+Bundler.require
+
+WebPay.api_key = 'test_secret_eHn4TTgsGguBcW764a2KA8Yd'
+WEBPAY_PUBLIC_KEY = 'test_public_19DdUs78k2lV8PO8ZCaYX3JT'
+WEBDB_PRESS_PRICE = 1554
+
+set :haml, format: :html5
+
+get '/' do
+  haml :index
+end
+
+post '/purchase' do
+  begin
+    @charge = WebPay::Charge.create(currency: 'jpy', amount: WEBDB_PRESS_PRICE, card: params['webpay-token'])
+    haml :purchased
+  rescue => e
+    redirect to('/')
+  end
+end
