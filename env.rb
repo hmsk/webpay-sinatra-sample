1c267ded087ff2b4313eb99a5917c5fc38f62fb0
hmsk <k.hamasaki@gmail.com>
2013-12-13 09:40:27 +0900

    Fix DB URL on Heroku

--- env.rb
+++ env.rb
@@ -3,7 +3,7 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_ONYX_URL'] || 'postgres:///localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
